63c2a2d0e55aff64abd3b6bed5227281e48d6089
sahniarushi <97777567+sahniarushi@users.noreply.github.com>
2022-02-19 09:45:25 +0530

    Update SingleNumber.java

--- SingleNumber.java
+++ SingleNumber.java
@@ -17,3 +17,17 @@ class Solution {
         
     }
 }
+
+class Solution {
+    public int singleNumber(int[] nums) {
+        int result = 0;
+        for(int i = 0;i<nums.length;i++)
+        {
+            result ^= nums[i]; // 1 ^ 1 = 0, 0 ^ x = x
+        }
+        
+        return result;
+    }
+}
+
+
